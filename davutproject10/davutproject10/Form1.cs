dde4be61b7b5005217a7a04ad01f91bab304154c
qngchan <QngChan@proton.me>
2025-12-13 18:51:37 +0300

    Takdir Alıp Almadığını Hesaplayan Program

---
+++ davutproject10/davutproject10/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace davutproject10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int ortalama;
+            ortalama = Convert.ToInt16(textBox1.Text);
+            if (ortalama < 85)
+            {
+                MessageBox.Show("Takdir Alamadın");
+
+            }
+            if (ortalama >= 85)
+            {
+                MessageBox.Show("Takdir Aldın");
+            }
+        }
+    }
+}
