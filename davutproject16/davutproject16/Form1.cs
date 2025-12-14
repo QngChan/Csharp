cfc6274af5dbf5b14428883fbbf33d3997737df1
qngchan <QngChan@proton.me>
2025-12-14 12:06:28 +0300

    İki Sayı Üzerinde Toplama ve Çarpma İşlemi
    Bu proje, C# Windows Forms kullanılarak geliştirilmiş, kullanıcıdan aldığı iki sayı üzerinde Toplama veya Çarpma işlemlerinden birini gerçekleştiren basit bir hesaplama aracıdır. Kullanıcı, işlemi ComboBox (açılır liste) bileşeni üzerinden seçer ve sonucu anında form üzerindeki bir Label'da görüntüler.

---
+++ davutproject16/davutproject16/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace davutproject16
+{
+    public partial class Form1 : Form
+    {
+   
+
+       
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+int deger1 = Convert.ToInt32(textBox1.Text);
+            int deger2 = Convert.ToInt32(textBox2.Text);
+            int sonuc;
+            if (comboBox1.Text == "Toplama")
+            {
+                label4.Text = (deger1 + deger2).ToString();
+                
+            }
+            else if (comboBox1.Text == "Çarpma")
+
+            {
+                label4.Text = (deger1 * deger2).ToString();
+            }
+        }
+    }
+}
