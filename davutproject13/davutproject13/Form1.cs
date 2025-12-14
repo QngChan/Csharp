0c225de70b5eda6eb246f670640dbb333fabb05c
qngchan <QngChan@proton.me>
2025-12-14 12:01:42 +0300

    Basit Kullanıcı Giriş Kontrol Uygulaması
    Bu C# Windows Forms uygulaması, temel düzeyde kullanıcı adı ve şifre doğrulaması yapmak üzere tasarlanmıştır. Uygulama, kullanıcının girdiği değerleri önceden tanımlanmış sabit değerlerle karşılaştırır ve sonucu kullanıcıya MessageBox aracılığıyla bildirir.

---
+++ davutproject13/davutproject13/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace davutproject13
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string kullanici_adi,sifre;
+            kullanici_adi = textBox1.Text;
+            sifre = textBox2.Text;
+            if (kullanici_adi == "milliegitim @meb.k12.tr")
+            {
+                MessageBox.Show("Kullanıcı sisteme kayıtlıdır.");
+            }
+            else
+            {
+                MessageBox.Show("Kullanıcı adınız yanlıştır.");
+            }
+            if (sifre == "12345")
+            {
+                MessageBox.Show("Şifre Doğru");
+            }
+            else
+            {
+                MessageBox.Show("Şifre Yanlış")
+            }
+        }
+    }
+}
