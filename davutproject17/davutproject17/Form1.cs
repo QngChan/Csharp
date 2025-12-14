95186997672afd093cbca4d09c856b3f9edace27
qngchan <QngChan@proton.me>
2025-12-14 12:08:26 +0300

    Sıcaklık Değerine Göre Hava Durumu
    Bu basit C# Windows Forms uygulaması, kullanıcıdan girilen sıcaklık değerini (Derece Cinsinden) analiz ederek hava durumunu üç farklı kategoriye ayırır ve sonucu bir uyarı kutusunda (MessageBox) görüntüler. Proje, temel karar yapıları (if koşulları) kullanılarak geliştirilmiştir.

---
+++ davutproject17/davutproject17/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace davutproject17
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int hava;
+            hava = Convert.ToInt32(textBox1.Text);
+            if (hava < 10)
+            {
+                MessageBox.Show("Hava Soğuk");
+            }
+            if (hava >= 10)
+            {
+                MessageBox.Show("Hava hafif sıcak");
+            }
+            if (hava > 25)
+            {
+                MessageBox.Show("Hava Sıcak");
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+} 
